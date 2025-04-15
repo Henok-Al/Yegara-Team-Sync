9d39bc6788b1a94518fdbe6878d54fddd4f55870
Henok-Al <henok.gebresenbet@gmail.com>
2025-04-16 01:09:23 +0300

    user route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,41 @@
 import "dotenv/config";
 import { config } from "./config/app.config";
 import connectDatabase from "./config/database.config";
+import userRoutes from "./routes/user.route";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware";
+import { NextFunction, Request, Response } from "express";
+import { BadRequestException } from "./utils/appError";
+import { ErrorCodeEnum } from "./enums/error-code.enum";
+import { HTTPSTATUS } from "./config/http.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
 
 const express = require("express");
+
 const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get(
+  `/`,
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    throw new BadRequestException(
+      "This is a bad request",
+      ErrorCodeEnum.AUTH_INVALID_TOKEN
+    );
+    return res.status(HTTPSTATUS.OK).json({
+      message: "Hello Subscribe to the channel & share",
+    });
+  })
+);
+
+app.use(`${BASE_PATH}/user`, userRoutes);
+
+app.use(errorHandler);
 
 app.listen(config.PORT, async () => {
   console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
-  await connectDatabase()
+  await connectDatabase();
 });
